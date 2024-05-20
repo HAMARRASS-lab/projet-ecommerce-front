caf76c864852917b901f2f759d83d9a3b8b3f9ca
hamza.hamarras <hamza.hamarras@inetum.com>
2024-05-20 14:32:11 +0100

    delete product

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AdminService } from './../../service/admin.service';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,14 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private AdminService: AdminService) { }
+  constructor(
+    private AdminService: AdminService, 
+    private fb:FormBuilder,
+    private snackBar: MatSnackBar,
+  ){}
+  products: any[] =[];
+  searchProductForm! :  FormGroup;
 
   ngOnInit(): void {
 
     this.getAllProducts();
-  }
+    this.searchProductForm=this.fb.group({
+         title: [null, [Validators.required]]
+    })
+   }
 
-  products: any[] =[];
+
+
 
   getAllProducts(){
     this.products=[];
@@ -24,9 +36,35 @@ export class DashboardComponent implements OnInit {
         element.processedImg='data:image/jpeg;base64,' +element.bytemg;
         this.products.push(element);
       });
+      console.log(this.products)
     })
 
   }
-  
+     submitForm(){
+      this.products=[];
+      const title= this.searchProductForm.get('title')!.value;
+      this.AdminService.getAllProductByName(title).subscribe(res=>{
+        res.forEach(element=>{
+          element.processedImg='data:image/jpeg;base64,' +element.bytemg;
+          this.products.push(element);
+        });
+        console.log(this.products)
+      })
+     }
+
+     deleteProduct(productId:any){
+      this.AdminService.deleteProduct(productId).subscribe(res=>{
+        if(res.body==null){
+          this.snackBar.open('Product Deleted Successfully', 'Close',{
+            duration:5000
+          })
+      }else{
+        this.snackBar.open(res.message, 'Close', {
+          duration: 5000,
+          panelClass: 'error-snackbar'
+        })
+      };
+     })
+    }
 
 }
